bca3e79aff28f178f6a79979508804a00f209d12
BarLesh <barlesh8@gmail.com>
2020-06-02 20:00:43 +0300

    responding to a request with parameters

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 
-def addBasic():
+
+basic_list = [] 
+def addBasic(body):
     print("addying basic")
+    basic_list.append(body)
+    print("basic_object: {}".format(body))
--- server.py
+++ server.py
@@ -1,5 +1,10 @@
 import connexion
+import os
 
-app = connexion.App(__name__, specification_dir='openAPI/')
-app.add_api('api.yaml')
-app.run(port=8080)
+spec_dir = os.environ.get("SPEC_DIR", 'openAPI/')
+spec_file = os.environ.get("SPEC_FILE", 'api.yaml')
+port = os.environ.get("PORT", 8080)
+
+app = connexion.App(__name__, specification_dir=spec_dir)
+app.add_api(spec_file)
+app.run(port=port)
