79cec660c08f2b41fb7677b939f8adf40b416b8b
BarLesh <barlesh8@gmail.com>
2020-06-06 23:05:48 +0300

    pytest

--- tests/api-tests/test_basic_api.py
+++ tests/api-tests/test_basic_api.py
@@ -7,6 +7,8 @@ import openapi_client
 from openapi_client.rest import ApiException
 from pprint import pprint
 
+import pytest
+
 # enviorment
 tested_host = os.environ.get("HOST", 'localhost')
 tested_port = os.environ.get("PORT", 8080)
@@ -18,21 +20,32 @@ configuration = openapi_client.Configuration(
     host=host
 )
 
-# Enter a context with an instance of the API client
-with openapi_client.ApiClient(configuration) as api_client:
-    # Create an instance of the API class
-    api_instance = openapi_client.BasicApi(api_client)
-    # Basic | Basic object that needs to be added to the basic stuff (optional)
-    body = openapi_client.Basic(id=1, description="whattt")
-    try:
-        # Add a basic stuff
-        ans = api_instance.src_basic_ctrl_basic_add_basic(body=body)
-    except ApiException as e:
-        pass
-        print("Exception when calling BasicApi->src_basic_ctrl_basic_add_basic: %s\n" % e)
 
-    ret = api_instance.src_basic_ctrl_basic_get_all()
-    print("ret: \n" + str(ret))
+class TestBasic:
+    def test_add_n_get_all(self):
+        uid = 1
+        description = "basic description"
+        expected = [{ "description" : description, "id" : uid }]
+        # Enter a context with an instance of the API client
+        with openapi_client.ApiClient(configuration) as api_client:
+            # Create an instance of the API class
+            api_instance = openapi_client.BasicApi(api_client)
+            # Basic | Basic object that needs to be added to the basic stuff (optional)
+            body = openapi_client.Basic(id=uid, description=description)
+            try:
+                # Add a basic stuff
+                ans = api_instance.src_basic_ctrl_basic_add_basic(body=body)
+            except ApiException as e:
+                pass
+                print("Exception when calling BasicApi->src_basic_ctrl_basic_add_basic: %s\n" % e)
+
+            ret = api_instance.src_basic_ctrl_basic_get_all()
+            print("ret:" + str(ret))
+            print("expected:" + str(expected))
+            assert isinstance(ret, list)
+            assert isinstance(ret[0], dict)
+            assert ret == expected
+            # self.assertDictEqual(ret, expected)
 
 
 
